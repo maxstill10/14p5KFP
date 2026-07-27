55ffacef49f2a974338da9db9da583b909cf8a19
LirikkV <zzxcawe.main@gmail.com>
2026-07-27 12:37:38 +0300

    Second commit

--- StRoot/StKFParticleAnalysisMaker/StKFParticleAnalysisMaker.cxx
+++ StRoot/StKFParticleAnalysisMaker/StKFParticleAnalysisMaker.cxx
@@ -748,6 +748,16 @@ Int_t StKFParticleAnalysisMaker::Make()
 
   cout<<"4444"<<endl;
 
+  for(int iProf=0; iProf!=10; iProf++){
+    for(int iSub=0; iSub!=nSub; iSub++){
+      cout<<Form("Psi1 : %i, %i", iProf, iSub)<<Coef_A_n_TH_Psi1[iProf][iSub]->GetBinContent(cent+1)*TMath::Cos((iProf+1)*Psi1[iSub]) + \
+                         Coef_B_n_TH_Psi1[iProf][iSub]->GetBinContent(cent+1)*TMath::Sin((iProf+1)*Psi1[iSub])<<endl;
+      
+      cout<<Form("Psi2 : %i, %i", iProf, iSub)<<Coef_A_n_TH_Psi2[iProf][iSub]->GetBinContent(cent+1)*TMath::Cos((iProf+1)*2*Psi2[iSub]) + \
+                         Coef_B_n_TH_Psi2[iProf][iSub]->GetBinContent(cent+1)*TMath::Sin((iProf+1)*2*Psi2[iSub])<<endl;
+    }
+  }
+
   
   //Flattaning
   for(int iProf=0; iProf!=10; iProf++){
